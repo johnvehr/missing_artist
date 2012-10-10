e25577e9818f472af39f089064e225f5813329bc
John Vehr <vehr@kyck.com>
2012-10-10 10:40:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MissingArtist::Application.config.secret_token = '7741ceddca60c50060564c13d69351ad068953838715ebfa2f86268ed7330c31da22815aa5abcfe95df042aed170fa500c701a7f31ff4f525ce2f0d952f8305f'
